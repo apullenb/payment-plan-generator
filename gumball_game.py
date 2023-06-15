67139f014f6707a650f2194e0d09d335704b1d1a
Andrea Pullen <apullenb@gmail.com>
2023-06-15 17:11:36 -0500

    update gumball game

--- gumball_game.py
+++ gumball_game.py
@@ -1,9 +1,9 @@
-def play_game(box_ids, goals, num_jars, min_gumballs, gumballs_per_jar):
+def play_game(box_ids, goals, num_jars, min_gumballs_list, gumballs_per_jar):
     num_boxes = len(box_ids)
     total_rounds = 0
 
     # Initialize the boxes with the minimum number of gumballs
-    boxes = {box_id: min_gumballs for box_id in box_ids}
+    boxes = {box_id: min_gumballs for box_id, min_gumballs in zip(box_ids, min_gumballs_list)}
 
     # Game loop
     while not all(g <= boxes[box_id] for box_id, g in zip(box_ids, goals)):
@@ -15,9 +15,9 @@ def play_game(box_ids, goals, num_jars, min_gumballs, gumballs_per_jar):
             for box_id in box_ids:
                 if boxes[box_id] < goals[box_ids.index(box_id)]:
                     diff = goals[box_ids.index(box_id)] - boxes[box_id]
-                    gumballs_added = min(diff, min_gumballs)
+                    gumballs_added = min(diff, boxes[box_id])
                     boxes[box_id] += gumballs_added
-                    min_gumballs -= gumballs_added
+                    boxes[box_id] -= gumballs_added
 
             # If there are extra gumballs, allocate them to boxes based on goal priority
             for box_id in sorted(box_ids, key=lambda x: goals[box_ids.index(x)]):
@@ -48,11 +48,11 @@ def play_game(box_ids, goals, num_jars, min_gumballs, gumballs_per_jar):
 box_ids = input("Enter the box IDs (comma-separated): ").split(",")
 goals = list(map(int, input("Enter the goal numbers for each box (comma-separated): ").split(",")))
 num_jars = int(input("Enter the number of jars: "))
-min_gumballs = int(input("Enter the minimum number of gumballs for each box: "))
+min_gumballs_list = list(map(int, input("Enter the minimum number of gumballs for each box (comma-separated): ").split(",")))
 gumballs_per_jar = int(input("Enter the number of gumballs per jar: "))
 
 # Play the game
-total_rounds = play_game(box_ids, goals, num_jars, min_gumballs, gumballs_per_jar)
+total_rounds = play_game(box_ids, goals, num_jars, min_gumballs_list, gumballs_per_jar)
 
 # Print game summary
 print("Game Over!")
